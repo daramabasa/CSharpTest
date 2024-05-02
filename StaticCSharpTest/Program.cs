ce98b90095e7003a54778469c5a740d001dc74f0
서혜원 <hwseo@suresofttech.com>
2024-05-02 16:45:21 +0900

    first

---
+++ StaticCSharpTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticCSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
